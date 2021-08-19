4fce74793e76b6f449e2287fc93f2134fc7e4156
noman-khalid-dev <noman.khalid.freelance@gmail.com>
2021-08-19 05:34:11 +0500

    Add home page

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Flight
 # Create your views here.
+
+
+def home(request):
+  return render (request,'flights/home.html')
+
 
 def index(request):
   return render(request,'flights/index.html',{
